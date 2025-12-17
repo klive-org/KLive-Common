d1afeb4373c815b86e3aea20d8233ac5549bdf42
darienkjc <darien@itkjc.com>
2025-12-17 15:33:36 +0700

    feat: add new actions to ConversationLogAction enum for agent room participation

--- src/enums/conversation-log-action.enum.ts
+++ src/enums/conversation-log-action.enum.ts
@@ -9,4 +9,6 @@ export enum ConversationLogAction {
   CONVERSATION_ENDED_BY_VISITOR = "conversation-ended-by-visitor",
   CONVERSATION_CLOSED_AUTOMATICALLY = "conversation-closed-automatically",
   CONVERSATION_TRANSFERRED_TO_OTHER_AGENT = "conversation-transferred-to-other-agent",
+  CONVERSATION_AGENT_JOIN_ROOM = "conversation-agent-join-room",
+  CONVERSATION_AGENT_LEAVE_ROOM = "conversation-agent-leave-room",
 }
