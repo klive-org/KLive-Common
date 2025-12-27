79cc7bd78b142604a197484d480426d8c3f7ac76
darienkjc <darien@itkjc.com>
2025-12-27 17:34:20 +0700

    .

--- dist/enums/index.d.ts
+++ dist/enums/index.d.ts
@@ -2,4 +2,6 @@ export * from "./conversation-event.enum";
 export * from "./conversation-log-action.enum";
 export * from "./form-submission-status.enum";
 export * from "./message-event.enum";
+export * from "./telegram-schedule.enum";
 export * from "./ws-event.enum";
+
