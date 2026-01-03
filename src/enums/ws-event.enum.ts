e809760b60abd3a3ef01edc4ee5080413ca33ab5
darienkjc <darien@itkjc.com>
2026-01-03 14:24:23 +0700

    refactor: Replace user online/offline ID events with user status changed and user offline events.

--- src/enums/ws-event.enum.ts
+++ src/enums/ws-event.enum.ts
@@ -1,6 +1,7 @@
 export enum ES2CWSEvent {
   WS_ERROR_RESPONSE = "s2c:ws-error-response",
-  RECV_ONLINE_ID = "s2c:recv-online-id",
-  RECV_OFFLINE_ID = "s2c:recv-offline-id",
+  // User status events - gửi kèm status đầy đủ
+  RECV_USER_STATUS_CHANGED = "s2c:recv-user-status-changed",
+  RECV_USER_OFFLINE = "s2c:recv-user-offline",
 }
 
