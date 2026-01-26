0e2b1530700428fcd283ba9e7a03ce927154365f
Jason KJC <jason@itkjc.com>
2026-01-26 14:30:40 +0700

    feat: Add end status for each conversation

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,3 +1,5 @@
+export * from "./agent-type.enum";
+export * from "./conversation-end-status.enum";
 export * from "./conversation-event.enum";
 export * from "./conversation-log-action.enum";
 export * from "./form-submission-status.enum";
@@ -5,4 +7,4 @@ export * from "./message-event.enum";
 export * from "./message-status.enum";
 export * from "./telegram-schedule.enum";
 export * from "./ws-event.enum";
-export * from "./agent-type.enum";
+
